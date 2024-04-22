92cd0547742e27921a68fb1937b4126f4b000225
kamilakamilkami <nurlybayevakamila@gmail.com>
2024-04-22 22:33:28 +0600

    kkkkk

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,18 @@ import Lists.MyArrayList;
 import Lists.MyList;
 
 
+public class Main {
+    public static void main(String[] args) {
+        MyList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(1, 0);
+        myArrayList.add(2, 1);
+        myArrayList.add(5);
+        System.out.println(myArrayList);
+
+        myArrayList.remove(0);
+        System.out.println(myArrayList);
+
+        System.out.println(myArrayList.get(2));
+    }
+
+}
