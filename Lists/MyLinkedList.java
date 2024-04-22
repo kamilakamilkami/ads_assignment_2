b06d7fd35b8c426f2ac06dd5d32a16ca77a50d15
kamilakamilkami <nurlybayevakamila@gmail.com>
2024-04-23 00:34:50 +0600

    salem alem

--- Lists/MyLinkedList.java
+++ Lists/MyLinkedList.java
@@ -1,4 +1,93 @@
 package Lists;
 
-public class MyLinkedList {
+public class MyLinkedList<T> implements MyList {
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public void addFirst(Object item) {
+
+    }
+
+    @Override
+    public void addLast(Object item) {
+
+    }
+
+    @Override
+    public void set(Object item, int index) {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object getFirst() {
+        return null;
+    }
+
+    @Override
+    public Object getLast() {
+        return null;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void removeFirst() {
+
+    }
+
+    @Override
+    public void removeLast() {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int indexOf(Object item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object item) {
+        return 0;
+    }
+
+    @Override
+    public boolean exists(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
